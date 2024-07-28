eab50b09306ab7178a52701317498fe053aa68a7
Anurag_1275 <165669312+Anurag1275@users.noreply.github.com>
2024-07-28 09:45:30 +0530

    Add files via upload

---
+++ project5.js
@@ -0,0 +1,24 @@
+const randomcolor=function(){
+    const hex="0123456789ABCDEF"
+    let color='#'
+    for(let i=0;i<6;i++){
+      color +=hex[Math.floor(Math.random()*16)]
+    }
+    return color;
+};
+let intervalid;
+
+const startchangingcolor=function(){
+    intervalid=setInterval(changeBgcolor,1000)
+    function changeBgcolor(){
+    document.body.style.backgroundColor=randomcolor()}
+
+};
+const stopchangingcolor=function(){
+    clearInterval(intervalid);
+    intervalid=null
+}; 
+
+document.querySelector('#start').addEventListener('click',startchangingcolor)
+document.querySelector('#stop').addEventListener('click',stopchangingcolor)
+
