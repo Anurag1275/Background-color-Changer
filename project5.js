a785b58dec8c606e0cf9c55dd3e8d8b871a354f9
Anurag_1275 <165669312+Anurag1275@users.noreply.github.com>
2024-09-17 21:18:43 +0530

    Update project5.js

--- project5.js
+++ project5.js
@@ -1,26 +1,28 @@
-const randomcolor=function(){
-    const hex="0123456789ABCDEF"
-    let color='#'
-    for(let i=0;i<6;i++){
-      color +=hex[Math.floor(Math.random()*16)]
+const randomcolor = function() {
+    const hex = "0123456789ABCDEF";
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)];
     }
     return color;
 };
-let intervalid;
 
-const startchangingcolor=function(){
-    intervalid=setInterval(changeBgcolor,1000)
-    function changeBgcolor(){
-    document.body.style.backgroundColor=randomcolor()}
+let intervalId;
 
+const changeBgcolor = function() {
+    document.body.style.backgroundColor = randomcolor();
 };
-const stopchangingcolor = function () {
-    if (intervalid) {
-        clearInterval(intervalid);
-        intervalid = null;
+
+const startchangingcolor = function() {
+    intervalId = setInterval(changeBgcolor, 1000);
+};
+
+const stopchangingcolor = function() {
+    if (intervalId) {
+        clearInterval(intervalId);
+        intervalId = null;
     }
 };
 
-document.querySelector('#start').addEventListener('click',startchangingcolor)
-document.querySelector('#stop').addEventListener('click',stopchangingcolor)
-
+document.querySelector('#start').addEventListener('click', startchangingcolor);
+document.querySelector('#stop').addEventListener('click', stopchangingcolor);
